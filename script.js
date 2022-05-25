5081a8ba8c57aaf9b834653e6686a519bdd6b1a0
dimebruce ⠀⠀⠀⠀⠀⠀ <>
2022-05-25 21:46:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let num1 = parseFloat(prompt("Ingresa el primer número: ", "El que quieras"));
+
+if( (num1%2) == 0){
+  console.log("El número "+num1+" es par");
+}
+else{
+  console.log("El número "+num1+" es impar");
+}
